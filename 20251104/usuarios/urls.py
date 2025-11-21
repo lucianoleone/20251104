00750dc1d3f0748890307c406a9cfaf144f10272
Luciano Leone <luciano.leone@gmail.com>
2025-11-21 15:23:37 -0300

    version ultima clase

--- 20251104/usuarios/urls.py
+++ 20251104/usuarios/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib import admin
-from usuarios.views import login, logout, register
+from usuarios.views import login, register
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/',login, name='login'),
-    path('logout/',logout, name='logout'),
-    path('register/',register, name='register')
+    path('login/', login, name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', register, name='register'),
 ]
--- 20251104/usuarios/views.py
+++ 20251104/usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 
 from inicio.models import Auto
@@ -9,8 +10,17 @@ from django.urls import reverse_lazy
 # Create your views here.
 
 def login(request):
-    formulario =AuthenticationForm()  # Aquí iría la lógica para manejar el formulario de login
-    return render(request, 'login.html',{formulario:'formulario'})
+    if request.method == 'POST':
+        formulario = AuthenticationForm(request, data=request.POST)
+        if formulario.is_valid(): #en la validacion ya hace el chequeo de la existencia del usuario y todo lo necesario para saber que le usuario es valido
+            usuario = formulario.get_user()
+            #logueamos al usuario
+            auth_login(request, usuario)
+            return redirect('inicio')
+    else:
+        formulario = AuthenticationForm()
+    return render(request, 'login.html', {"formulario": formulario})
+
 def logout(request):
     return render(request, 'usuarios/logout.html')
 def register(request):
